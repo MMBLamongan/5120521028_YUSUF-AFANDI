0dd1f9f07c76f9648dc59615fe7dfa22d419b18b
MMBLamongan <mmbpsdkulamongan@gmail.com>
2023-07-13 19:53:39 +0700

    first commit

---
+++ Assets/LoadScenesB.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenesB : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("LoadSceneB");
+    }
+
+    public void LoadB(int sceneANumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+        SceneManager.LoadScene(sceneANumber);
+    }
+}
